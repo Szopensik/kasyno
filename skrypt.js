b2f0d2d77902ca6302f29a7d928f4ae159e9dab6
Szopensik <bereznickifranek@gmail.com>
2026-03-18 09:41:24 +0100

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -6,9 +6,34 @@ let konto = document.querySelector(".stan")
 let wiecej = document.querySelector(".doladuj")
 wiecej.addEventListener("click", doladowanie)
 przycisk.addEventListener("click", gra)
+ const divy = [div1, div2, div3];
+ const muzyka = new Audio("music.mp3");
+ muzyka.volume = 0.3;
+muzyka.loop = true;
+muzyka.play();
 let kasa=1000
+let pitty = 0
 function sleep(ms) {                 
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+function wingif() {
+  const gif = document.getElementById("winGif");
+
+  gif.style.display = "block";
+
+  setTimeout(() => {
+    gif.style.display = "none";
+  }, 9000);
+}
+
+function wingifcoin() {
+  const coin = document.getElementById("wingifcoin");
+
+  coin.style.display = "block";
+
+  setTimeout(() => {
+    coin.style.display = "none";
+  }, 9000);
 }
 
 function playSound() {
@@ -16,9 +41,22 @@ function playSound() {
   audio.play().catch(err => console.error("Nie udało się odtworzyć dźwięku:", err));
 }
 
+function winsound() {
+  const audio = new Audio("bigwin.mp3"); 
+  audio.play().catch(err => console.error("Nie udało się odtworzyć dźwięku:", err));
+}
+
+function littlewin() {
+  const audio = new Audio("winnita-casino.mp3"); 
+  audio.play().catch(err => console.error("Nie udało się odtworzyć dźwięku:", err));
+}
+
 async function gra() {
+    kasa=kasa-50
+     konto.textContent="Stan konta: " + kasa + "$"
+  if (pitty<5) {
     przycisk.disabled = true;
-  const divy = [div1, div2, div3];
+ 
   for (let i = 0; i < divy.length; i++) {
     divy[i].classList.add("spin");
     
@@ -33,11 +71,75 @@ async function gra() {
       case 4: divy[i].style.backgroundImage = "url(orange.png)"; break;
       case 5: divy[i].style.backgroundImage = "url(slot-machine.png)"; break;
     }
-
+    
     divy[i].classList.remove("spin");
+    pitty=pitty+1
   }
   przycisk.disabled = false;
+  }
+
+  else{
+    przycisk.disabled = true;
+
+  for (let i = 0; i < divy.length; i++) {
+    divy[i].classList.add("spin");
+    
+    playSound();
+    await sleep(1200);
+
+    let l = 2
+    switch (l) {
+      case 1: divy[i].style.backgroundImage = "url(bell.png)"; break;
+      case 2: divy[i].style.backgroundImage = "url(casino.png)"; break;
+      case 3: divy[i].style.backgroundImage = "url(gambling.png)"; break;
+      case 4: divy[i].style.backgroundImage = "url(orange.png)"; break;
+      case 5: divy[i].style.backgroundImage = "url(slot-machine.png)"; break;
+    }
+    
+    divy[i].classList.remove("spin");
+    pitty=0
+  }
+  przycisk.disabled = false;
+  }
+  
+
+
+
+
+let wynik = divy.every(val =>
+  val.style.backgroundImage.includes(
+    divy[0].style.backgroundImage.match(/url\(["']?(.*?)["']?\)/)[1]
+  )
+);
+
+if (wynik) {
+  if (div1.style.backgroundImage.includes("casino.png")) {
+    console.log("big win");
+    winsound();
+    kasa += 5000;
+    wingif()
+    wingifcoin()
+  } else {
+    console.log("lekki win");
+    littlewin();
+    kasa += 100;
+  }
+  konto.textContent="Stan konta: " + kasa + "$"
 }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
 konto.textContent="Stan konta: " + kasa + "$"
 function doladowanie() {
  let blik = 0
@@ -52,3 +154,6 @@ konto.textContent="Stan konta: " + kasa + "$"
 
 
 }
+
+
+
